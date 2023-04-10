f885a580a4bf765385320f2ff0e24ae26c5656b7
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-04-10 07:18:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -19,6 +19,14 @@ Employee.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
 
+const person = new Person('Alice', 30);
+person.greet(); 
+
+
+const employee = new Employee('Bob', 25, 'Software Engineer');
+employee.jobGreet(); 
+
+
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
