32f89f8786f6f8afb851c103e653fd41a0d3153c
Arun kumar <106857719+ArunKumar023@users.noreply.github.com>
2023-04-10 07:29:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ function Person(name, age) {
   this.name = name;
   this.age = age;
 }
-
 Person.prototype.greet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
 };
+
 
 function Employee(name, age, jobTitle) {
   Person.call(this, name, age);
